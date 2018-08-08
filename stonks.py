79d13f414577f852b69fa8d99c645416dfa29eed
Ethan Windows <ethan.t.schmidt@gmail.com>
2018-08-08 09:44:32 -0400

    asdf

--- stonks.py
+++ stonks.py
@@ -15,14 +15,19 @@ all_data = quandl.get_table('WIKI/PRICES', ticker = ['AAPL', 'MSFT', 'GOOG', 'IB
                         date = { 'gte': '2015-12-31', 'lte': '2016-12-31' }, 
                         paginate=True)
 
+print(all_data)
+
 # Isolate the `Adj Close` values and transform the DataFrame
-daily_close_px = all_data['adj_close'].reset_index().pivot('Date', 'ticker', 'adj_close')
+daily_close_px = all_data.pivot(index='date', columns='ticker', values='adj_close')
 
 # Calculate the daily percentage change for `daily_close_px`
 daily_pct_change = daily_close_px.pct_change()
 
 # Plot the distributions
-daily_pct_change.hist(bins=50, sharex=True, figsize=(12,8))
+# daily_pct_change.hist(bins=50, sharex=True, figsize=(12,8))
+
+# plot a scatter matrix
+pd.plotting.scatter_matrix(daily_pct_change, diagonal='kde', alpha=0.1, figsize=(12,12))
 
 # Show the resulting plot
 plt.show()
